86790836b8c5224954169b4e6f1e0c52a47489a6
Hati <lambertthomas20cents@gmail.com>
2025-05-22 21:27:45 +0200

    Creation sous programme ConvertMtoMH

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    public static (int, int) ConvertMToHM(int nbMin)
+    {
+        return (nbMin / 60, nbMin % 60);
+    }
+
+    private static void Main(string[] args)
+    {
+        // Programme principal
+        (int H, int M) = ConvertMToHM(130);
+        Console.WriteLine($"{H}:{M}");
+    }
+}
